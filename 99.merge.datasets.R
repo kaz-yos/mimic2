ef2c7830848f815fdbe47654ac45c8472f0aea6c
kaz-yos <kazky@mac.com>
2014-01-05 16:50:11 -0500

    preliminary analysis

--- 99.merge.datasets.R
+++ 99.merge.datasets.R
@@ -25,9 +25,11 @@ outputDataset <- orderBy(~  +SUBJECT_ID, outputDataset)
 ## Primary dataset first rows only
 dataPrimary <- read.csv("~/mimic2/chrisData/primary.csv")
 dataPrimary <- dataPrimary[!duplicated(dataPrimary$SUBJECT_ID),]
-dataPrimary <- dataPrimary[c("SUBJECT_ID","SAPSI_FIRST","ICUSTAY_EXPIRE_FLG")]
+dataPrimary <- dataPrimary[c("SUBJECT_ID","SAPSI_FIRST","ICUSTAY_EXPIRE_FLG","AGE")]
 
 dataPrimary$ICUSTAY_EXPIRE_FLG <- as.character(factor(dataPrimary$ICUSTAY_EXPIRE_FLG, levels = c("N","Y"), labels = c(1,0)))
+## convert to numerics
+dataPrimary$ICUSTAY_EXPIRE_FLG <- as.numeric(dataPrimary$ICUSTAY_EXPIRE_FLG)
 table(dataPrimary$ICUSTAY_EXPIRE_FLG)
 
 
@@ -71,9 +73,30 @@ finalData <- merge(x = outputDataset,
 dput(names(finalData))
 
 
-colOrder <- c("SUBJECT_ID", "ICUSTAY_ID", "HR", "vent", "sat", "SAPSI_FIRST",  "TEMPERATURE", "CONGESTIVE_HEART_FAILURE", "CARDIAC_ARRHYTHMIAS", "VALVULAR_DISEASE", "PULMONARY_CIRCULATION", 
-"PERIPHERAL_VASCULAR", "HYPERTENSION", "PARALYSIS", "OTHER_NEUROLOGICAL", "DIABETES_COMPLICATED", "DIABETES_UNCOMPLICATED", "HYPOTHYROIDISM", "RENAL_FAILURE", "LIVER_DISEASE", "PEPTIC_ULCER", "AIDS", "LYMPHOMA", "METASTATIC_CANCER", "SOLID_TUMOR", "RHEUMATOID_ARTHRITIS", "COAGULOPATHY", "OBESITY", "WEIGHT_LOSS", "FLUID_ELECTROLYTE", "BLOOD_LOSS_ANEMIA", 
-"DEFICIENCY_ANEMIAS", "ALCOHOL_ABUSE", "DRUG_ABUSE", "PSYCHOSES", "DEPRESSION", "WBC", "ICUSTAY_EXPIRE_FLG")
+## Check NA's
+summary(finalData)
+
+## Keep original
+finalDataOrig <- finalData
+
+## Mean imputation
+finalData[,c(-1,-2,-7)] <- sapply(finalDataOrig[,c(-1,-2,-7)],
+                      FUN = function(VAR) {
+
+                          ## Get the mean
+                          meanVal <- mean(VAR, na.rm = T)
+
+                          ## NA replaced with the mean
+                          VAR[is.na(VAR)] <- meanVal
+
+                          ## Return it
+                          VAR
+                      },
+                      simplify = FALSE)
+
+
+## Choose ordering
+colOrder <- c("SUBJECT_ID", "AGE", "HR", "vent", "sat", "SAPSI_FIRST",  "TEMPERATURE", "CONGESTIVE_HEART_FAILURE", "CARDIAC_ARRHYTHMIAS", "VALVULAR_DISEASE", "PULMONARY_CIRCULATION", "PERIPHERAL_VASCULAR", "HYPERTENSION", "PARALYSIS", "OTHER_NEUROLOGICAL", "DIABETES_COMPLICATED", "DIABETES_UNCOMPLICATED", "HYPOTHYROIDISM", "RENAL_FAILURE", "LIVER_DISEASE", "PEPTIC_ULCER", "AIDS", "LYMPHOMA", "METASTATIC_CANCER", "SOLID_TUMOR", "RHEUMATOID_ARTHRITIS", "COAGULOPATHY", "OBESITY", "WEIGHT_LOSS", "FLUID_ELECTROLYTE", "BLOOD_LOSS_ANEMIA", "DEFICIENCY_ANEMIAS", "ALCOHOL_ABUSE", "DRUG_ABUSE", "PSYCHOSES", "DEPRESSION", "WBC", "ICUSTAY_EXPIRE_FLG")
 
 
 ## Final dataset!!!!
@@ -81,3 +104,22 @@ finalData <- finalData[colOrder]
 
 ## write out!!!!!
 write.csv(finalData, "~/mimic2/data__final.csv", row.names = F, na = "")
+
+
+## Write out as Excel
+## http://d.hatena.ne.jp/dichika/20131003/p1
+library(XLConnect)
+wb <- loadWorkbook("data__final.XLConnect.xls", create = TRUE)
+wb["1"] <- finalData
+saveWorkbook(wb)
+
+
+
+### Logistic regression (analysis is four lines of code!!!!???)
+resGlm <- glm(formula = ICUSTAY_EXPIRE_FLG ~ . -SUBJECT_ID -BLOOD_LOSS_ANEMIA,
+              family  = binomial(link = "logit"),
+              data    = finalData)
+summary(resGlm)
+
+## library(epicalc)
+## epicalc::logistic.display(resGlm)
